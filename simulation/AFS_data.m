72a615f8310b75e2427ee3623c2d6e7dd0faefb3
ashkan1997 <ashkan.behmanesh@studenti.unitn.it>
2023-06-06 18:55:53 +0200

    6/6/2023

--- simulation/AFS_data.m
+++ simulation/AFS_data.m
@@ -1,8 +1,8 @@
 function AFS_data = AFS_data()
     
-    X      = 1;  % controller gain
+    X      = 0;  % controller gain
     lambda = 1;  % positive constant
-    ro    = 1.5;
+    ro    = 0.5;
 
     AFS_data.X      = X;
     AFS_data.lambda = lambda;
