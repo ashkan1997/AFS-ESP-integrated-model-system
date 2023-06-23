2c166c96362532222792f60898534566aeecbeb8
ashkan1997 <ashkan.behmanesh@studenti.unitn.it>
2023-06-23 16:50:42 +0200

    23/jun/2023

--- simulation/open_simulink_model.m
+++ simulation/open_simulink_model.m
@@ -4,7 +4,7 @@
 
 % Check if the Simulink model is already opened. Otherwise open it
 openModels = find_system( 'SearchDepth', 0 );
-if ( isempty( find( strcmp( openModels, 'single_track' ), 1 ) ) )
-    load_system( 'single_track.slx' );
-    open_system( 'single_track' );
+if ( isempty( find( strcmp( openModels, 'combined' ), 1 ) ) )
+    load_system( 'combined.slx' );
+    open_system( 'combined' );
 end
