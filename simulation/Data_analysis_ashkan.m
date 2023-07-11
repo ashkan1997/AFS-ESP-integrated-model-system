b3b7854525faf675f0bde985ce7ebcba6fc5b11f
ashkan1997 <ashkan.behmanesh@studenti.unitn.it>
2023-07-11 10:10:31 +0200

    Final- 10/7/2023

--- simulation/Data_analysis_ashkan.m
+++ simulation/Data_analysis_ashkan.m
@@ -37,22 +37,23 @@ time = model_sim.tout;
 
 
 %% Yaw rate
-figure( 'Name','Yaw rate', 'PaperSize',[125 125])
-plot(time , r_NC , 'LineWidth', 1.5 , LineStyle='-.' ,DisplayName='No Controller')
-hold on
-plot(time , r_AFS , 'LineWidth', 1.5 , LineStyle='--' ,DisplayName='AFS')
-hold on
-plot(time , r_comb , 'LineWidth', 2 , 'DisplayName', ' AFS + ESP' , 'LineStyle','-' , 'Color', [0.518 0.635 0.286])
-hold on
-plot(time , r_ESP , 'LineWidth', 1.5 , LineStyle=':' ,DisplayName='ESP')
-hold on
-% fontsize(gca,33,"pixels")
-legend()
-xlabel('Time', 'FontWeight','bold')
-ylabel('$\Omega$ [rad/s]', 'FontWeight','bold')
+figure( 'Name','Yaw rate')
 
+plot(time , r_NC , 'LineWidth', 4 , LineStyle='-.' ,DisplayName='No Controller')
+hold on
+plot(time , r_AFS , 'LineWidth', 4 , LineStyle='--' ,DisplayName='AFS')
+hold on
+plot(time , r_comb , 'LineWidth', 6 , 'DisplayName', ' AFS + ESP' , 'LineStyle','-' , 'Color', [0.518 0.635 0.286])
+hold on
+plot(time , r_ESP , 'LineWidth', 4 , LineStyle=':' ,DisplayName='ESP')
+hold on
+fontsize(gca,33,"pixels")
+legend('FontSize',24)
+xlabel('Time', 'FontWeight','bold' , 'FontSize',35)
+ylabel('$\Omega$ [rad/s]', 'FontWeight','bold', 'FontSize',35)
 grid off
 fig = gcf;
+
 
 % exportgraphics(fig,['figure/' scenario_name '_Omega.eps']);
 % annotation("textbox" , 'String', {['k1= ' , num2str(vehicle_data.AFS.k1)] ...
@@ -68,21 +69,22 @@ fig = gcf;
 %% side slip angle (beta)
 
 figure('Name','Beta')
-plot(time , beta_NC , 'LineWidth', 1 , LineStyle='-.' ,DisplayName='No Controller')
+plot(time , beta_NC , 'LineWidth', 3 , LineStyle='-.' ,DisplayName='No Controller')
 hold on
-plot(time , beta_AFS , 'LineWidth', 1.1 , LineStyle='--' ,DisplayName='AFS')
+plot(time , beta_AFS , 'LineWidth', 3 , LineStyle='--' ,DisplayName='AFS')
 hold on
-plot(time , beta_comb , 'LineWidth', 1.5 , 'DisplayName', ' AFS + ESP' , 'LineStyle','-' , 'Color',  [0.518 0.635 0.286])
+plot(time , beta_comb , 'LineWidth', 5 , 'DisplayName', ' AFS + ESP' , 'LineStyle','-' , 'Color',  [0.518 0.635 0.286])
 hold on
-plot(time , beta_ESP , 'LineWidth', 1 , LineStyle=':' ,DisplayName='ESP')
+plot(time , beta_ESP , 'LineWidth', 3 , LineStyle=':' ,DisplayName='ESP')
 hold on
-legend
-xlabel('Time', 'FontWeight','bold' , 'FontSize',25)
-ylabel('$\beta$ [rad/s]', 'FontWeight','bold' , 'FontSize',25)
-grid on
+fontsize(gca,33,"pixels")
+legend('FontSize',24)
+xlabel('Time', 'FontWeight','bold' , 'FontSize',35)
+ylabel('$\beta$ [rad/s]', 'FontWeight','bold' , 'FontSize',35)
+grid off
 
 fig = gcf;
-exportgraphics(fig,['figure/' scenario_name '_beta.eps']);
+% exportgraphics(fig,['figure/' scenario_name '_beta.eps']);
 % annotation("textbox" , 'String', {['k1= ' , num2str(vehicle_data.AFS.k1)] ...
 %                                 ['k2= ' , num2str(vehicle_data.AFS.k2)] ...
 %                                 ['B1= ' , num2str(vehicle_data.AFS.B1)] ...
@@ -104,12 +106,13 @@ plot(cond_test , rho_c , 'DisplayName' , 'Rho_c' , 'LineWidth',2)
 hold on
 plot(cond_test , 1-rho_c , 'DisplayName' , '1-Rho_c' , 'LineStyle', '--' , 'LineWidth',2)
 hold on
-legend
-grid on
-xlabel('$\kappa_{1} \dot{\beta} + \kappa_{2} \beta$' , 'FontWeight','bold' , 'FontSize',25)
-ylabel('$\rho_{c}$', 'FontWeight','bold' , 'FontSize',25)
+fontsize(gca,33,"pixels")
+legend('FontSize',24)
+grid off
+xlabel('$\kappa_{1} \dot{\beta} + \kappa_{2} \beta$' , 'FontWeight','bold' , 'FontSize',35)
+ylabel('$\rho_{c}$', 'FontWeight','bold' , 'FontSize',35)
 fig = gcf;
-exportgraphics(fig,['figure/' scenario_name '_Activation_Fun.eps']);
+% exportgraphics(fig,['figure/' scenario_name '_Activation_Fun.eps']);
 
 
 %% trajectory
